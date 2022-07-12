8782c494127b8496381fe8119beb573a9d2f96ea
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-07-12 14:39:33 +0900

    config: eslint setting, only use arrow function

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,12 @@ module.exports = {
     project: './tsconfig.json',
   },
   rules: {
+    'react/prop-types': 'off',
+    // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
     'react/react-in-jsx-scope': 'off',
     'import/order': [
       'error',
